3a3cb61fe74a29737d054795d5f2fd6fa7dd0c16
WOLFSilverWolf <ginnefirimu@gmail.com>
2016-05-12 00:16:46 +0200

    Added some JavaScript for the subscribing cards.

--- pages/abonnement/abonnement.js
+++ pages/abonnement/abonnement.js
@@ -1,15 +1,31 @@
 (function () {
-  'use strict';
-  const toggleElement = document.querySelector('.icon-2x');
-  const toggle = function (event) {
-    const target = event.target;
-    if (target instanceof HTMLSpanElement) {
-      if (target.className === 'icon-toggle-off icon-2x') {
-        target.className = 'icon-toggle-on icon-2x';
-      } else {
-        target.className = 'icon-toggle-off icon-2x';
-      }
-    }
-  };
-  toggleElement.addEventListener('click', toggle);
+    'use strict';
+    const cards = document.querySelectorAll('.card');
+    const toggleElement = document.querySelector('.icon-2x');
+    const toggle = function (event) {
+        const target = event.target;
+        if (target instanceof HTMLSpanElement) {
+            if (target.className === 'icon-toggle-off icon-2x') {
+                target.className = 'icon-toggle-on icon-2x';
+            }
+            else {
+                target.className = 'icon-toggle-off icon-2x';
+            }
+        }
+    };
+    const selectCard = function () {
+        if (this instanceof HTMLDivElement) {
+            const currentBackgroundColor = getComputedStyle(this, null).getPropertyValue("background-color");
+            this.style.backgroundColor = currentBackgroundColor.replace(/(\d\.\d+)/, '0.72');
+        }
+    };
+    const addEventsToNodeList = function (nodeList, index) {
+        index -= 1;
+        if (index >= 0) {
+            nodeList[index].addEventListener('click', selectCard);
+            addEventsToNodeList(nodeList, index);
+        }
+    };
+    addEventsToNodeList(cards, cards.length);
+    toggleElement.addEventListener('click', toggle);
 })();
--- pages/abonnement/abonnement.ts
+++ pages/abonnement/abonnement.ts
@@ -1,16 +1,34 @@
 (function () {
   'use strict';
-    const toggleElement = document.querySelector('.icon-2x');
-    
+    const cards: NodeList = document.querySelectorAll('.card');
+    const toggleElement: Element = document.querySelector('.icon-2x');
+
     const toggle = function (event: MouseEvent): void {
-    const target = event.target;
-    if (target instanceof HTMLSpanElement) {
-      if (target.className === 'icon-toggle-off icon-2x') {
-        target.className = 'icon-toggle-on icon-2x';
-      } else {
-        target.className = 'icon-toggle-off icon-2x';
+      const target: EventTarget = event.target;
+      if (target instanceof HTMLSpanElement) {
+        if (target.className === 'icon-toggle-off icon-2x') {
+          target.className = 'icon-toggle-on icon-2x';
+        } else {
+          target.className = 'icon-toggle-off icon-2x';
+        }
       }
-    }
-  };
+    };
+
+    const selectCard = function (): void {
+      if (this instanceof HTMLDivElement) {
+        const currentBackgroundColor: string = getComputedStyle(this, null).getPropertyValue("background-color");
+	this.style.backgroundColor = currentBackgroundColor.replace(/(\d\.\d+)/, '0.72');
+      }
+    };
+
+    const addEventsToNodeList = function (nodeList: NodeList, index: number): void {
+      index -= 1;
+      if (index >= 0) {
+        nodeList[index].addEventListener('click', selectCard);
+        addEventsToNodeList(nodeList, index);
+      }
+    };
+
+  addEventsToNodeList(cards, cards.length);
   toggleElement.addEventListener('click', toggle);
 })();
