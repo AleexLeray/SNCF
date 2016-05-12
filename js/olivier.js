f168b8897b00e27ff98a5358f90501d0d8720023
Asmaodeus <cadoz.olivier@gmail.com>
2016-05-12 10:29:49 +0200

    angular minified

--- js/olivier.js
+++ js/olivier.js
@@ -1,1 +1,32 @@
-var app = angular.module("shopApp", []);
+
+
+
+
+(function(){
+  var app = angular.module("shopApp", []);
+
+  app.controller("StoreController", function(){
+    this.products = livres;
+  });
+
+  var livres =[
+    {
+    image: "images/essai.png",
+    titre: "L'humanisme, une religion?",
+    resume: "description",
+    prix: 500
+    },
+    {
+    image: "images/lite.png",
+    titre: "Passages à l'étranger",
+    resume: "description",
+    prix: 231
+    },
+    {
+    image: "images/polar.png",
+    titre: "Recherche cadavre exquis",
+    resume: "description",
+    prix: 957
+    }
+  ];
+})();
