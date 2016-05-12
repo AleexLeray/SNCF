dfcbade83b5ae1ef19befecb75bda002db1476c8
Asmaodeus <cadoz.olivier@gmail.com>
2016-05-12 12:47:03 +0200

    cartes finies

--- js/olivier.js
+++ js/olivier.js
@@ -5,14 +5,14 @@
 (function(){
   var app = angular.module("shopApp", []);
 
-  app.controller("StoreController", function(){
+  app.controller("StoreController", function($scope){
     this.products = livres;
   });
 
-  var livres =[
+   var livres =[
     {
-    image: "images/essai.png",
-    titre: "L'humanisme, une religion?",
+    image: "images/giratoire.png",
+    titre: "Giratoire",
     resume: "description",
     prix: 500
     },
@@ -27,6 +27,36 @@
     titre: "Recherche cadavre exquis",
     resume: "description",
     prix: 957
-    }
+    },
+    {
+    image: "images/plage.png",
+    titre: "Une semaine légèrement agitée",
+    resume: "description",
+    prix: 500
+    },
+    {
+    image: "images/scifi.png",
+    titre: "La zizique de l'âme",
+    resume: "description",
+    prix: 500
+    },
+    {
+    image: "images/lgdm.png",
+    titre: "La guerre des mondes",
+    resume: "description",
+    prix: 500
+    },
+    {
+    image: "images/historique.png",
+    titre: "Moi Jean-Thierry, Héritier Tipaldy",
+    resume: "description",
+    prix: 500
+    },
+    {
+    image: "images/louis.png",
+    titre: "La Malentente",
+    resume: "description",
+    prix: 500
+    },
   ];
 })();
